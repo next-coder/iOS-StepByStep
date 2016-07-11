4f3e10d7050d9e68fd6cee9a13d69b46bf0ad9cb
Jasper <huangqisheng1@xiaoniu66.com>
2016-07-11 14:57:39 +0800

    new Objective-c CollectionView project

---
+++ Objc/NextCoder088-UICollectionView/NextCoder088-UICollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NextCoder088-UICollectionView
+//
+//  Created by xn011644 on 7/11/16.
+//  Copyright © 2016 Next-coder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
